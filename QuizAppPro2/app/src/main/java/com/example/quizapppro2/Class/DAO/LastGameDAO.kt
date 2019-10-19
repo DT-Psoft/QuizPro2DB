f0438743239ccbc63aa623a40d0fcfe9b66384c3
PKfede <47314404+PKfede@users.noreply.github.com>
2019-10-18 20:13:34 -0500

    DAO's incompletos

---
+++ QuizAppPro2/app/src/main/java/com/example/quizapppro2/Class/DAO/LastGameDAO.kt
@@ -0,0 +1,12 @@
+package com.example.quizapppro2.Class.DAO
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.quizapppro2.Class.Entities.LastGameETY
+
+@Dao
+interface LastGameDAO {
+}
