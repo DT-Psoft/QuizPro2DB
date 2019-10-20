78d6fcd2dba400240fbccac2936c4779bd583061
Kike <daurixg@gmailo.com>
2019-10-20 16:43:54 -0500

    update de user

--- QuizAppPro2/app/src/main/java/com/example/quizapppro2/Class/Entities/UserETY.kt
+++ QuizAppPro2/app/src/main/java/com/example/quizapppro2/Class/Entities/UserETY.kt
@@ -5,10 +5,9 @@ import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "user",indices = [Index(value = ["id_user"], unique = true)])
+@Entity(tableName = "user",indices = [Index(value = ["id_user", "user_name"], unique = true)])
 data class UserETY (
     @field:ColumnInfo(name = "user_name") var user_name: String,
-    @field:ColumnInfo(name = "password") var password: Int,
     @field:ColumnInfo(name = "is_logged") var is_logged: Int = 0,
     @field:ColumnInfo(name = "image_user") var image_user: Int
 ){
